8d9105d61b3eb6b9d2b800c96e7cc7770b7bb011
Peter Gafert <peter.gafert@tngtech.com>
2020-03-01 22:30:47 +0100

    add support for simple class type parameters
    Introduce `JavaTypeVariable` to model type parameters like `MyClass<T>`. For now the model is quite simple, it only supports type bounds that are not generic themselves. In fact the current implementation will behave wrongly, as soon as there are types like `MyClass<T extends SomeGeneric<AnotherType>>`. However the current state already shows correct behavior for simple cases like `MyClass<T extends SomeClass & SomeInterface, U extends YetAnotherClass & More & Interfaces>`.
    
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

--- archunit/src/test/java/com/tngtech/archunit/testutil/assertion/JavaTypeAssertion.java
+++ archunit/src/test/java/com/tngtech/archunit/testutil/assertion/JavaTypeAssertion.java
@@ -1,18 +1,26 @@
 package com.tngtech.archunit.testutil.assertion;
 
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Strings;
+import com.google.common.collect.FluentIterable;
 import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaModifier;
 import com.tngtech.archunit.core.domain.JavaType;
+import com.tngtech.archunit.core.domain.JavaTypeVariable;
 import org.assertj.core.api.AbstractObjectAssert;
 import org.objectweb.asm.Type;
 
+import static com.tngtech.archunit.base.Guava.toGuava;
+import static com.tngtech.archunit.core.domain.properties.HasName.Predicates.name;
+import static com.tngtech.archunit.testutil.TestUtils.namesOf;
 import static com.tngtech.archunit.testutil.assertion.JavaAnnotationAssertion.propertiesOf;
 import static com.tngtech.archunit.testutil.assertion.JavaAnnotationAssertion.runtimePropertiesOf;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.guava.api.Assertions.assertThat;
 
 public class JavaTypeAssertion extends AbstractObjectAssert<JavaTypeAssertion, JavaType> {
     private static final Pattern ARRAY_PATTERN = Pattern.compile("(\\[+)(.*)");
@@ -48,6 +56,26 @@ public class JavaTypeAssertion extends AbstractObjectAssert<JavaTypeAssertion, J
         if (clazz.isArray()) {
             new JavaTypeAssertion(javaClass.getComponentType()).matches(clazz.getComponentType());
         }
+    }
+
+    public JavaTypeAssertion hasTypeParameters(String... names) {
+        assertThat(namesOf(actualClass().getTypeParameters())).as("names of type parameters").containsExactly(names);
+        return this;
+    }
+
+    @SuppressWarnings("OptionalGetWithoutIsPresent") // checked via AssertJ
+    public JavaTypeVariableOfClassAssertion hasTypeParameter(String name) {
+        List<JavaTypeVariable> typeVariables = actualClass().getTypeParameters();
+
+        Optional<JavaTypeVariable> variable = FluentIterable.from(typeVariables).firstMatch(toGuava(name(name)));
+        assertThat(variable).as("Type variable with name '%s'", name).isPresent();
+
+        return new JavaTypeVariableOfClassAssertion(variable.get(), this);
+    }
+
+    public JavaTypeVariableOfClassAssertion hasOnlyTypeParameter(String name) {
+        assertThat(actualClass().getTypeParameters()).as("Type parameters").hasSize(1);
+        return hasTypeParameter(name);
     }
 
     private JavaClass actualClass() {
